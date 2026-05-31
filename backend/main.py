d0438611cea52f3c6c5b6e1a4368b15e4a0a7c1d
tut-svaga <serreal1021@gmail.com>
2026-05-31 15:28:48 +0300

    r

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ class JsonWeather(BaseModel):
 # ЯДРО: Единая функция запроса погоды по коордам
 # ==========================================
 async def fetch_weather(client: httpx.AsyncClient, lat: float, lon: float, city_name: str) -> JsonWeather:
-    weather_url = "https://api.open-meteo.com/v1/forecast"
+    weather_url = "https://weather-backend-cayx.onrender.com"
     params = {
         "latitude": lat,
         "longitude": lon,
